d351020a82e5d4dfbf22714c54406aeae5d32844
hizzang920 <60869831+hizzang920@users.noreply.github.com>
2021-01-11 19:27:27 +0900

    Q.20

---
+++ 20.Valid Parentheses_ver2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        open_bracket = ['{', '[', '('] 
+        close_bracket = ['}', ']', ')'] 
+        
+        for i in s:
+            if i in open_bracket:
+                stack.append(i) 
+            elif i in close_bracket:
+                if len(stack) <= 0:
+                    return False
+                if open_bracket.index(stack.pop()) != close_bracket.index(i):
+                    return False
+        return len(stack) == 0
